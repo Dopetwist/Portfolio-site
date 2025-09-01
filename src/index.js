c6c8eb4ae51da8027eda986b51a979c4872b3c71
Jonathan <jonathanafugwobi1@gmail.com>
2025-09-01 18:59:53 +0100

    Add previously built projects images to folder and js array

--- src/index.js
+++ src/index.js
@@ -3,35 +3,35 @@ const projects = [
     {
         title: "BookHive",
         description: "A Node.js web app with secured authentication that helps users document individual reading history by storing personal notes, summaries and books ratings to a database.",
-        img: "",
+        img: "/images/projects/bookhive.png",
         github: "https://github.com/Dopetwist/BookHive.git",
         tech: [ "Node.js", "Express.js", "JavaScript", "PostgreSQL", "HTML", "CSS", "EJS" ]
     },
     {
         title: "Food Recipes Project",
         description: "This project allows users to explore and search varieties of foreign dishes all over the world with preparation instructions.",
-        img: "",
+        img: "/images/projects/recipe.png",
         github: "https://github.com/Dopetwist/Foreign-recipes-project.git",
         tech: [ "RESTful API", "Node.js", "JavaScript", "EJS", "Express.js", "HTML", "CSS" ]
     },
     {
         title: "GeoMindz",
         description: "A simple and interactive full stack quiz application that challenges users to match countries with their capitals. It was built with a focus on a clean, responsive UI and smooth quiz experience.",
-        img: "",
+        img: "/images/projects/geomindz.png",
         github: "https://github.com/Dopetwist/GeoMindz.git",
-        tech: [ "React.js", "Node.js", "PostgreSQL", "JavaScript", "Express.js", "HTML", "CSS" ]
+        tech: [ "React.js + Vite", "Node.js", "PostgreSQL", "JavaScript", "Express.js", "HTML", "CSS" ]
     },
     {
         title: "PS4 Controller",
         description: "I designed and built a complete Gamepad with code. Flexed my fundamental skills on this project for an amazing output.",
-        img: "",
+        img: "/images/projects/gamepad.png",
         github: "https://github.com/Dopetwist/PS4_Controller.git",
         tech: [ "HTML", "CSS" ]
     },
     {
         title: "Sleek Unisex Salon",
         description: "This website showcases awesome male and female hairstyles, services and products.",
-        img: "",
+        img: "/images/projects/sleek.png",
         github: "https://github.com/Dopetwist/Sleek-salon.git",
         tech: [ "React.js + Vite", "JavaScript", "HTML", "CSS" ]
     }
