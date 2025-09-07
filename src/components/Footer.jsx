b475a36a0b2d463773002fe97919da980a1f916b
Jonathan <jonathanafugwobi1@gmail.com>
2025-09-07 16:27:52 +0100

    Update linkedin profile URL, add target attributes and reduce Footer paragraph font size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,25 +7,25 @@ function Footer() {
 
             <div className="socials">
                 
-                <a href="www.linkedin.com/in/jonathan-afugwobi-b960a0264" className="linkedin"> 
+                <a href="https://www.linkedin.com/in/jonathan-afugwobi-b960a0264" className="linkedin" target="blank"> 
                     <div className="social-con">
                         <img src="/images/socials/linkedin.png" alt="LinkedIn Image" />
                     </div>
                 </a>
             
-                <a href="https://github.com/Dopetwist" className="github"> 
+                <a href="https://github.com/Dopetwist" className="github" target="blank"> 
                     <div className="social-con">
                         <img src="/images/socials/github.jpg" alt="GitHub Image" />
                     </div>
                 </a>
 
-                <a href="https://www.instagram.com/dopetwist001" className="instagram"> 
+                <a href="https://www.instagram.com/dopetwist001" className="instagram" target="blank"> 
                     <div className="social-con">
                         <img src="/images/socials/instagram.jpeg" alt="Instagram Image" />
                     </div>
                 </a>
 
-                <a href="https://x.com/dopetwist001" className="X"> 
+                <a href="https://x.com/dopetwist001" className="X" target="blank"> 
                     <div className="social-con">
                         <img src="/images/socials/X.jpg" alt="X Image" />
                     </div>
