516ebda7669421cc5f2b8aa4d0063f02b4ff4474
Jonathan <jonathanafugwobi1@gmail.com>
2025-09-07 16:13:40 +0100

    Add profile links to Footer social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,25 +7,25 @@ function Footer() {
 
             <div className="socials">
                 
-                <a href="#" className="linkedin"> 
+                <a href="www.linkedin.com/in/jonathan-afugwobi-b960a0264" className="linkedin"> 
                     <div className="social-con">
                         <img src="/images/socials/linkedin.png" alt="LinkedIn Image" />
                     </div>
                 </a>
             
-                <a href="#" className="github"> 
+                <a href="https://github.com/Dopetwist" className="github"> 
                     <div className="social-con">
                         <img src="/images/socials/github.jpg" alt="GitHub Image" />
                     </div>
                 </a>
 
-                <a href="#" className="instagram"> 
+                <a href="https://www.instagram.com/dopetwist001" className="instagram"> 
                     <div className="social-con">
                         <img src="/images/socials/instagram.jpeg" alt="Instagram Image" />
                     </div>
                 </a>
 
-                <a href="#" className="X"> 
+                <a href="https://x.com/dopetwist001" className="X"> 
                     <div className="social-con">
                         <img src="/images/socials/X.jpg" alt="X Image" />
                     </div>
