f2d52e1405ded9759877800c256960d8ebcb4bdb
Jonathan <jonathanafugwobi1@gmail.com>
2025-09-16 01:55:37 +0100

    Start responsiveness optimization

--- src/components/ScrollReveal.jsx
+++ src/components/ScrollReveal.jsx
@@ -8,14 +8,16 @@ const ScrollWrapper = () => {
             duration: 1500,
             easing: 'ease-in-out',
             origin: 'left',
-            delay: 100
+            delay: 100,
+            reset: true
         }),
         ScrollReveal().reveal('.home-text', {
             distance: '10rem',
             duration: 2000,
             easing: 'ease-in-out',
             origin: 'right',
-            delay: 100
+            delay: 100,
+            reset: true
         }),
         ScrollReveal().reveal('#about-img', {
             distance: '5rem',
