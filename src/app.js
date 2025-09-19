3cf744b0755bc134e9fb6bcec13bfee1fc64dafa
Jonathan <jonathanafugwobi1@gmail.com>
2025-09-19 03:45:11 +0100

    Complete Side Bar functionality

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@
 
 window.addEventListener("scroll", () => {
     const navbar = document.getElementById("header");
+    const menu = document.getElementById("menu-icon");
+    const nav = document.querySelector(".nav-links");
 
     if (window.scrollY > 0) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
     }
+
+    menu.classList.remove("bx-x");
+    nav.classList.remove("toggle");
+    nav.classList.remove("show");
 });
 
 
