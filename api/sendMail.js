d18eabafc684bf5d1eb651f7629eb0d3e2ded94c
Jonathan <jonathanafugwobi1@gmail.com>
2025-09-20 16:21:36 +0100

    Add Email Automation logic code

---
+++ api/sendMail.js
@@ -0,0 +1,35 @@
+
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
+    }
+
+    const { name, email, message } = req.body;
+
+    try {
+        // Transporter config
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+
+        // Send email
+        await transporter.sendMail({
+            from: email,
+            to: process.env.EMAIL_USER,
+            subject: `New Portfolio Contact: ${name}`,
+            text: message,
+        });
+
+        return res.status(200).json({ success: true, msg: "Email sent!" });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Failed to send email" });
+    }
+}
