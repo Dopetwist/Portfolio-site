129747e39f298ebe83236c3c60fffc26bfa11e19
Jonathan <jonathanafugwobi1@gmail.com>
2025-09-25 15:35:45 +0100

    Install Icons package. Create Icons Component and import necessary icons

---
+++ src/components/Icons.jsx
@@ -0,0 +1,37 @@
+import { 
+    TextAlignJustify, 
+    Sun, 
+    Moon, 
+    ChevronsDown, 
+    Download, 
+    CodeXml, 
+    ArrowRight, 
+    ArrowUp,
+    Send,
+    MoveRight,
+    MoveLeft,
+    Mail,
+    Phone,
+    MapPin,
+
+} from "lucide-react";
+
+
+const Icons = {
+    TextAlignJustify,
+    Sun,
+    Moon, 
+    ChevronsDown, 
+    Download, 
+    CodeXml, 
+    ArrowRight, 
+    ArrowUp,
+    Send,
+    MoveRight,
+    MoveLeft,
+    Mail,
+    Phone,
+    MapPin
+}
+
+export default Icons;
